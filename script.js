7d1f123fa655e8f56ed0f9b31f92512b907c515e
Jerrod Jaygbay <jerrodjaygbay123@gmail.com>
2026-01-30 23:10:56 -0500

    fixed some things with my physical tab

--- script.js
+++ script.js
@@ -168,7 +168,8 @@ navItems.forEach(item => {
         
         console.log(`Switched to: ${page}`);
         
-        if (page !== 'chat') {
+        const isLink = item.tagName.toLowerCase() === 'a' || item.hasAttribute('href');
+        if (page !== 'chat' && !isLink) {
             alert(`${item.querySelector('.nav-title').textContent} - Coming soon!`);
         }
     });
